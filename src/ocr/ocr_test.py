b5a4b2baf423dfd5598bcfe716657f5daed651c5
Anirudh Raghavan <anirudhraghavan2002@gmail.com>
2026-08-12 13:08:53 -0400

    Add initial EasyOCR prototype

---
+++ src/ocr/ocr_test.py
@@ -0,0 +1,16 @@
+import easyocr
+
+print("Loading OCR model...")
+
+reader = easyocr.Reader(['en'])
+
+print("Reading image...")
+
+results = reader.readtext("data/raw_images/1.jpg")
+
+print("\nDetected Text:\n")
+
+for bbox, text, confidence in results:
+    print(f"Text: {text}")
+    print(f"Confidence: {confidence:.2f}")
+    print("-" * 40)
